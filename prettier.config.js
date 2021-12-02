e4e573b3bfb016333f777d7a64c2477ad721f677
Nick <chidojiro@gmail.com>
2021-12-03 04:20:34 +0700

    feat: init tools

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{ts,tsx}'],
+  darkMode: false, // or 'media' or 'class',
+  theme: {},
+  plugins: [],
+};
