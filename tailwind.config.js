e4e16b3708126cfb44a51e3340b27d6601a5fb20
Nick <chidojiro@gmail.com>
2021-12-03 07:16:20 +0700

    chore: remove tailwindcss leftover

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{ts,tsx}'],
-  darkMode: false, // or 'media' or 'class',
-  theme: {},
-  plugins: [],
-};
