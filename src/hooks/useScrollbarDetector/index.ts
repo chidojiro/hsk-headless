ca4810d155eca7a095337130c225e0ea14e21009
Nick <chidojiro@gmail.com>
2022-07-06 16:13:04 +0700

    feat: useScrollbarDetector

---
+++ src/hooks/useScrollbarDetector/index.ts
@@ -0,0 +1,1 @@
+export { useScrollbarDetector } from './useScrollbarDetector'
---
+++ src/hooks/useScrollbarDetector/useScrollbarDetector.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+import { HTMLElementOrHTMLElementRef } from 'types';
+import { AssertUtils } from 'utils';
+
+export const useScrollbarDetector = (elementOrElementRef: HTMLElementOrHTMLElementRef, deps: any[]) => {
+  const [scrollbarWidth, setScrollbarWidth] = React.useState(0);
+
+  React.useEffect(() => {
+    const element = AssertUtils.isRef<HTMLElement>(elementOrElementRef)
+      ? elementOrElementRef.current
+      : elementOrElementRef;
+
+    setScrollbarWidth((element?.offsetWidth ?? 0) - (element?.clientWidth ?? 0));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [...deps, elementOrElementRef]);
+
+  return React.useMemo(() => ({ scrollbarWidth }), [scrollbarWidth]);
+};
