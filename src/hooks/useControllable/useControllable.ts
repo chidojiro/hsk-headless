02d66a1a42f041ef5999d36bcc2468b1e6240086
Nick <chidojiro@gmail.com>
2022-07-06 16:16:54 +0700

    fix: remove valueAs and changeAs from useControllable

--- src/hooks/useControllable/useControllable.ts
+++ src/hooks/useControllable/useControllable.ts
@@ -1,22 +1,14 @@
-import isFunction from 'lodash/isFunction';
-import { Fn } from './../../types';
+import { isFunction } from 'lodash-es';
 import React from 'react';
+import { Fn } from 'types';
 
 export type UseControllableProps<T = any> = {
   value?: T;
   defaultValue?: T;
   onChange?: (e: any) => void;
-  valueAs?: (value: T) => any;
-  changeAs?: (value: any) => T;
 };
 
-export const useControllable = <T>({
-  value: valueProp,
-  onChange,
-  defaultValue,
-  valueAs,
-  changeAs,
-}: UseControllableProps<T>): any => {
+export const useControllable = <T>({ value: valueProp, onChange, defaultValue }: UseControllableProps<T>): any => {
   const isControlled = valueProp !== undefined && valueProp !== null;
   const prevValueRef = React.useRef(valueProp);
 
@@ -40,18 +32,16 @@ export const useControllable = <T>({
   const UncontrolledState = React.useMemo(() => [value, _setValue], [_setValue, value]);
 
   if (isControlled) {
-    const _value = valueAs ? valueAs(valueProp as any) : valueProp;
-
     const _setValue = (e: any) => {
       let _newValue = e;
 
       if (isFunction(e)) _newValue = e(prevValueRef.current);
 
-      onChange?.(changeAs ? changeAs(_newValue) : _newValue);
+      onChange?.(_newValue);
       prevValueRef.current = _newValue;
     };
 
-    return [_value, _setValue];
+    return [valueProp, _setValue];
   }
 
   return UncontrolledState;
