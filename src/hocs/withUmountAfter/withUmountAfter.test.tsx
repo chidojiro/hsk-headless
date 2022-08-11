9ffb61c5ec9f63d8e2ce4c57e05ae15d3924ec05
Nick <chidojiro@gmail.com>
2022-08-11 22:55:00 +0700

    fix: update withUnmountAfter test

--- src/hocs/withUmountAfter/withUmountAfter.test.tsx
+++ src/hocs/withUmountAfter/withUmountAfter.test.tsx
@@ -1,35 +1,28 @@
 import React from 'react';
 import { render, act } from '@testing-library/react';
 
-import { useMountEffect } from 'hooks';
 import { withUmountAfter } from './withUmountAfter';
 
-const mockUnmountCallback = jest.fn();
-
-const UnmountEffectHost = () => {
-  useMountEffect(() => () => mockUnmountCallback());
-
-  return null;
+const Component = () => {
+  return <div>hello world</div>;
 };
 
-const UNMOUNT_AFTER = 1000;
+const renderComponent = (unmountAfter: number) => {
+  const ComponentWrapper = withUmountAfter(unmountAfter)(Component);
 
-const renderComponent = (unmountAfter = UNMOUNT_AFTER) => {
-  const Component = withUmountAfter(unmountAfter)(UnmountEffectHost);
-
-  render(<Component />);
+  return render(<ComponentWrapper />);
 };
 
-it('should call unmount callback after 1000ms', () => {
-  renderComponent();
+it('should unmount after 1000ms', () => {
+  const { queryByText } = renderComponent(1000);
 
-  expect(mockUnmountCallback).not.toBeCalled();
+  expect(queryByText('hello world')).toBeInTheDocument();
   act(() => {
     jest.advanceTimersByTime(999);
   });
-  expect(mockUnmountCallback).not.toBeCalled();
+  expect(queryByText('hello world')).toBeInTheDocument();
   act(() => {
     jest.advanceTimersByTime(1);
   });
-  expect(mockUnmountCallback).toBeCalled();
+  expect(queryByText('hello world')).not.toBeInTheDocument();
 });
