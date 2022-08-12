dc17978fc3dcf11fbc2408694aa557c96316fa2e
Nick <chidojiro@gmail.com>
2022-08-12 14:30:46 +0700

    fix: clear timeout effect in withUnmountAfter

--- src/hocs/withUmountAfter/withUmountAfter.tsx
+++ src/hocs/withUmountAfter/withUmountAfter.tsx
@@ -5,14 +5,19 @@ export const withUmountAfter =
   <TProps,>(Component: (props: TProps) => JSX.Element | null) =>
   // eslint-disable-next-line react/display-name
   (props: TProps) => {
+    const timeoutRef = React.useRef<NodeJS.Timeout>();
     const [shouldMount, setShouldMount] = React.useState(true);
 
     React.useEffect(() => {
       if (unmountAfter) {
-        setTimeout(() => {
+        timeoutRef.current = setTimeout(() => {
           setShouldMount(false);
         }, unmountAfter);
       }
+
+      return () => {
+        clearTimeout(timeoutRef.current);
+      };
     }, []);
 
     if (shouldMount) return <Component {...props} />;
