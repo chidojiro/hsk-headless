a4ea3242e719665cd9ed1b237bfd8634cc370d32
Nick <chidojiro@gmail.com>
2023-03-17 19:31:39 +0700

    fix: use Controller instead of useController

--- src/components/Form/Field.tsx
+++ src/components/Form/Field.tsx
@@ -1,12 +1,11 @@
-import { useMountEffect } from '@/hooks';
-import { get } from 'lodash-es';
 import { ChangeEventHandler, ComponentType, FocusEventHandler, forwardRef, useCallback } from 'react';
-import { RegisterOptions, useController, useFormContext } from 'react-hook-form';
+import { Controller, RegisterOptions, useFormContext } from 'react-hook-form';
 
 type FieldOwnProps<TComponentProps, TValue> = {
   component: ComponentType<TComponentProps>;
   name: string;
   rules?: RegisterOptions;
+  value?: any;
   emptyValue?: TValue;
   valueAs?: (value: any) => any;
   changeAs?: (value: any) => any;
@@ -29,42 +28,17 @@ const ForwardedField = <TComponentProps, TValue>(
     onChange: onChangeProp,
     valueAs = (value: TValue) => value,
     changeAs = (value: TValue) => value,
+    value: valueProp,
     ...restProps
   }: FieldProps<TComponentProps, TValue>,
   ref: any
 ) => {
   const Component = component as any;
 
-  const {
-    formState: { errors },
-    setValue,
-  } = useFormContext();
-
-  const {
-    field: { onChange, onBlur, value, ref: fieldRef, ...restField },
-  } = useController({ name, rules });
-
-  useMountEffect(() => {
-    if (emptyValue && !value) {
-      setValue(name, emptyValue, { shouldDirty: false });
-    }
-  });
-
-  const error = get(errors, name);
-
-  const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
-    const _value = changeAs ? changeAs(e) : e ?? emptyValue;
-    onChange(_value);
-    onChangeProp?.(e);
-  };
-
-  const handleBlur: FocusEventHandler<HTMLInputElement> = e => {
-    onBlur();
-    onBlurProp?.(e);
-  };
+  const { control } = useFormContext();
 
   const resolveValue = useCallback(
-    () => {
+    (value: any) => {
       if ([null, undefined].includes(value)) {
         return emptyValue;
       }
@@ -76,23 +50,45 @@ const ForwardedField = <TComponentProps, TValue>(
       return value;
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [JSON.stringify(emptyValue), JSON.stringify(value), valueAs]
+    [JSON.stringify(emptyValue), valueAs]
     /* eslint-enable react-hooks/exhaustive-deps */
   );
 
-  const hasError = !!error || undefined;
-
   return (
-    <Component
-      onChange={handleChange}
-      onBlur={handleBlur}
-      error={hasError}
-      className={className}
-      value={resolveValue()}
-      ref={ref ?? fieldRef}
-      placeholderInputRef={fieldRef}
-      {...restField}
-      {...(restProps as any)}
+    <Controller
+      name={name}
+      control={control}
+      rules={rules}
+      render={({ field: { name, onBlur, onChange, ref: fieldRef, value }, fieldState: { error } }) => {
+        const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
+          const _value = changeAs ? changeAs(e) : e ?? emptyValue;
+          onChange(_value);
+          onChangeProp?.(e);
+        };
+
+        const handleBlur: FocusEventHandler<HTMLInputElement> = e => {
+          onBlur();
+          onBlurProp?.(e);
+        };
+
+        if (emptyValue && !value) {
+          onChange(name, emptyValue, { shouldDirty: false });
+        }
+
+        return (
+          <Component
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={error}
+            className={className}
+            value={resolveValue(value)}
+            ref={ref ?? fieldRef}
+            checked={resolveValue(value) === valueProp}
+            name={name}
+            {...(restProps as any)}
+          />
+        );
+      }}
     />
   );
 };
